86b747ea023fccfdbdec49694f9ba45b292fe411
Specas <shrenik95@gmail.com>
2016-01-27 23:56:00 +0530

    Servo PID regulator 1

--- pid_1.ino
+++ pid_1.ino
@@ -2,31 +2,48 @@
 
 Servo servo1;
 
+// Servo pwm pin
 int servo_pin = 9;
 
 
-
-void pid_servo(int servo_pin, int final_angle)
+// Function that turns the servo to an angle final_angle
+void pid_servo(int final_angle)
 {
   
+  //Initializing parameters
 
+  //Errors
   int prev_error = final_angle;
   int curr_error = final_angle;
+  
+  // Instantaneous error differential
   float e_differential = final_angle;
+  
+  // Current position of the servo
   int pos = 0;
+
+  // PID regulator parameters
   float kp = 0.3;
   float ki = 0.2;
   float kd = 3;
+
+  // Slice of time taken
   float dt = 15.0;
+
+  // Integral of the errors at all instances of time till the present time
   float e_integral = e_differential*dt;
-  float e_diff = 0.0;
   float u = 0.0;
  
   
   while(curr_error!=0)
   {
+    // Delay
     delay(dt);
+
+    // updating position
     pos +=1;
+
+    // Writing servo position depending on the control signal from the PID regulator output
     servo1.write(pos+((int)u%(300)/(int)dt));
 
     // Differential part
@@ -40,10 +57,10 @@ void pid_servo(int servo_pin, int final_angle)
     // Final control signal
     u = (kp*curr_error) + (kd*e_differential) + (ki*e_integral);
 
-    // Adjusting pos with control signal
-//    pos = pos + int(floor(((int)u%600)/dt));
+    // The control signal does not change the value of 'pos' directly. Instead, its value is added to the value that the servo must be turned to 
+    // at that instant of time
 
-    
+    // For debugging
     Serial.println(u);
 
 
@@ -53,12 +70,18 @@ void pid_servo(int servo_pin, int final_angle)
 
 void setup()
 {
+  // Begin serial monitor for debugging purposes
   Serial.begin(9600);
+
+  // Attaching the servo to the pwm pin
   servo1.attach(servo_pin);
+  
   // Set initial angle to zero
   servo1.write(0);
   delay(2000);
-  pid_servo(9, 180);
+
+  // PID control to turn the motor by an angle
+  pid_servo(180);
   
 }
 
